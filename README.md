1a3520688d8291a8b268e07778cb71c10c9b109a
Sandro Mund <sandro.mund@consistec.de>
2024-03-11 10:43:45 +0100

    Added mlflow tracking

--- README.md
+++ README.md
@@ -5,10 +5,16 @@ Learn more about it [here](https://realpython.com/generative-adversarial-network
 
 Training
 ```shell
- python src/main.py train
+ python src/main.py train --epochs 100
 ```
 
 Generate
 ```shell
 python src/main.py generate --model generator.pt
+```
+Tracking
+MlFlow Server on http://localhost:5000
+```shell
+mlflow server --host 127.0.0.1 --port 8080
+
 ```
--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+import datetime
+
 import torch
 from matplotlib import pyplot as plt
 from torch import nn
@@ -6,59 +8,73 @@ from tqdm import tqdm
 from data import get_demo_data_loader
 from model import Generator, Discriminator
 
+import mlflow.pytorch
+
 
 def train_models(batch_size, model_save_path, lr, num_epochs, plot_training):
-    discriminator = Discriminator()
-    generator = Generator()
-    train_loader = get_demo_data_loader(batch_size=batch_size)
-    loss_function = nn.BCELoss()
+    mlflow.set_experiment("Generative Adversarial Network")
+    mlflow.pytorch.autolog()
 
-    optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=lr)
-    optimizer_generator = torch.optim.Adam(generator.parameters(), lr=lr)
+    with mlflow.start_run() as run:
 
-    loss_discriminator_list = []
-    loss_generator_list = []
+        mlflow.log_param('batch_size', batch_size)
+        mlflow.log_param('learning_rate', lr)
+        mlflow.log_param('num_epochs', num_epochs)
 
-    for epoch in tqdm(range(num_epochs)):
-        for n, (real_samples, _) in enumerate(train_loader):
-            # Data for training the discriminator
-            real_samples_labels = torch.ones((batch_size, 1))
-            latent_space_samples = torch.randn((batch_size, 2))
-            generated_samples = generator(latent_space_samples)
-            generated_samples_labels = torch.zeros((batch_size, 1))
-            all_samples = torch.cat((real_samples, generated_samples))
-            all_samples_labels = torch.cat(
-                (real_samples_labels, generated_samples_labels)
-            )
+        discriminator = Discriminator()
+        generator = Generator()
+        train_loader = get_demo_data_loader(batch_size=batch_size)
+        loss_function = nn.BCELoss()
 
-            # Training the discriminator
-            discriminator.zero_grad()
-            output_discriminator = discriminator(all_samples)
-            loss_discriminator = loss_function(
-                output_discriminator, all_samples_labels)
-            loss_discriminator.backward()
-            optimizer_discriminator.step()
+        optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=lr)
+        optimizer_generator = torch.optim.Adam(generator.parameters(), lr=lr)
 
-            # Data for training the generator
-            latent_space_samples = torch.randn((batch_size, 2))
+        loss_discriminator_list = []
+        loss_generator_list = []
 
-            # Training the generator
-            generator.zero_grad()
-            generated_samples = generator(latent_space_samples)
-            output_discriminator_generated = discriminator(generated_samples)
-            loss_generator = loss_function(
-                output_discriminator_generated, real_samples_labels
-            )
-            loss_generator.backward()
-            optimizer_generator.step()
+        for epoch in tqdm(range(num_epochs)):
+            for n, (real_samples, _) in enumerate(train_loader):
+                # Data for training the discriminator
+                real_samples_labels = torch.ones((batch_size, 1))
+                latent_space_samples = torch.randn((batch_size, 2))
+                generated_samples = generator(latent_space_samples)
+                generated_samples_labels = torch.zeros((batch_size, 1))
+                all_samples = torch.cat((real_samples, generated_samples))
+                all_samples_labels = torch.cat(
+                    (real_samples_labels, generated_samples_labels)
+                )
 
-            loss_discriminator_list.append(float(loss_discriminator))
-            loss_generator_list.append(float(loss_generator))
+                # Training the discriminator
+                discriminator.zero_grad()
+                output_discriminator = discriminator(all_samples)
+                loss_discriminator = loss_function(
+                    output_discriminator, all_samples_labels)
+                loss_discriminator.backward()
+                optimizer_discriminator.step()
 
-    if plot_training:
-        plt.plot(loss_discriminator_list, label='loss_discriminator')
-        plt.plot(loss_generator_list, label='loss_generator')
-        plt.legend()
-        plt.show()
+                # Data for training the generator
+                latent_space_samples = torch.randn((batch_size, 2))
 
-    torch.save(generator.state_dict(), model_save_path)
+                # Training the generator
+                generator.zero_grad()
+                generated_samples = generator(latent_space_samples)
+                output_discriminator_generated = discriminator(generated_samples)
+                loss_generator = loss_function(
+                    output_discriminator_generated, real_samples_labels
+                )
+                loss_generator.backward()
+                optimizer_generator.step()
+
+                loss_discriminator_list.append(float(loss_discriminator))
+                loss_generator_list.append(float(loss_generator))
+
+                mlflow.log_metric("loss_discriminator", loss_discriminator, step=epoch)
+                mlflow.log_metric("loss_generator", loss_generator, step=epoch)
+
+        if plot_training:
+            plt.plot(loss_discriminator_list, label='loss_discriminator')
+            plt.plot(loss_generator_list, label='loss_generator')
+            plt.legend()
+            plt.show()
+
+        torch.save(generator.state_dict(), model_save_path)
