5b5782e114146f66389a46223f3bc347740b7766
Sandro Mund <sandro.mund@consistec.de>
2024-03-14 10:00:00 +0100

    Added plot_data_sample

--- src/visualize.py
+++ src/visualize.py
@@ -5,6 +5,22 @@ import numpy as np
 from const import ONE_HOT_ENCODING
 
 
+def plot_data_sample(sample):
+    """
+    Example:
+        from data import Preprocessor
+        from visualize import plot_data_sample
+
+        pp = Preprocessor()
+        data = pp.preprocess_data(data_path="../data/climbs")
+        pp.info()
+        plot_data_sample(data[0][0])
+    """
+    track_x = np.argmax(sample.detach().cpu().numpy(), axis=-1)
+    plt.imshow(track_x, interpolation='none', cmap=cm.binary)
+    plt.show()
+
+
 def plot_generated_sample(sample):
     if ONE_HOT_ENCODING:
         track_x = np.argmax(sample.detach().cpu().numpy()[0], axis=-1)
