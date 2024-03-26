a20211b92eab3fb0f9a174900c118ef139d12711
Sandro <31536101+sandromund@users.noreply.github.com>
2024-03-26 17:49:05 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn
 from tqdm import tqdm
 
-from const import LATENT_SPACE_SAMPLE, ONE_HOT_ENCODING
+from const import LATENT_SPACE_SAMPLE, ONE_HOT_ENCODING, BETAS
 from data import get_data_loader
 from model import Generator, Discriminator
 
@@ -22,14 +22,15 @@ def train_models(data_path, batch_size, lr, num_epochs):
         mlflow.log_param('num_epochs', num_epochs)
         mlflow.log_param("LATENT_SPACE_SAMPLE".lower(), LATENT_SPACE_SAMPLE)
         mlflow.log_param("ONE_HOT_ENCODING".lower(), ONE_HOT_ENCODING)
+        mlflow.log_param("BETAS".lower(), BETAS)
 
         discriminator = Discriminator().to(device)
         generator = Generator().to(device)
         train_loader = get_data_loader(data_path=data_path, batch_size=batch_size)
-        loss_function = nn.BCELoss().to(device)
+        loss_function = nn.BCELoss().to(device)  # binary cross entropy loss function
 
-        optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=lr)
-        optimizer_generator = torch.optim.Adam(generator.parameters(), lr=lr)
+        optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=BETAS)
+        optimizer_generator = torch.optim.Adam(generator.parameters(), lr=lr, betas=BETAS)
 
         for epoch in tqdm(range(num_epochs), "train_models"):
             for n, (real_samples, _) in enumerate(train_loader):
@@ -62,6 +63,7 @@ def train_models(data_path, batch_size, lr, num_epochs):
 
                 mlflow.log_metric("loss_discriminator", loss_discriminator, step=epoch)
                 mlflow.log_metric("loss_generator", loss_generator, step=epoch)
+                mlflow.log_metric("generated_samples_sum", generated_samples.sum() / batch_size, step=epoch)
 
         mlflow.pytorch.log_model(generator, "generator")
         mlflow.pytorch.log_model(discriminator, "discriminator")
