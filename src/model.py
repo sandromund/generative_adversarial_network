2f66d70b1257f9733d52cf498ad1beb75c1f65b1
Sandro <31536101+sandromund@users.noreply.github.com>
2024-03-29 21:25:55 +0100

    New Model architectures

--- src/model.py
+++ src/model.py
@@ -1,24 +1,28 @@
 import torch
 from torch import nn
 
-from const import LATENT_SPACE_SAMPLE, DATA_DIMENSION, ONE_HOT_ENCODING
+from const import LATENT_SPACE_SAMPLE, DATA_DIMENSION, ONE_HOT_ENCODING, DISCRIMINATOR_ROUND_OUTPUT
 
 
 class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(DATA_DIMENSION, 648),
-            nn.LeakyReLU(),
+            nn.Linear(DATA_DIMENSION, 1296),
+            nn.LeakyReLU(0.2),
+            nn.Dropout(0.2),
+            nn.Linear(1296, 324),
+            nn.LeakyReLU(0.2),
             nn.Dropout(0.5),
-            nn.Linear(648, 1),
-            nn.Sigmoid(),
+            nn.Linear(324, 1),
+            nn.Sigmoid()
         )
 
     def forward(self, x):
         x = x.view(x.size(0), DATA_DIMENSION)
         output = self.model(x)
-        output = torch.round(output)
+        if DISCRIMINATOR_ROUND_OUTPUT:
+            output = torch.round(output)
         return output
 
 
@@ -26,16 +30,11 @@ class Generator(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(LATENT_SPACE_SAMPLE, 648),
-            nn.LeakyReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(648, 648),
-            nn.LeakyReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(648, 648),
-            nn.LeakyReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(648, DATA_DIMENSION),
+            nn.Linear(LATENT_SPACE_SAMPLE, 36),
+            nn.LeakyReLU(0.2),
+            nn.Linear(36, 144),
+            nn.LeakyReLU(0.2),
+            nn.Linear(144, DATA_DIMENSION),
         )
 
     def forward(self, x):
