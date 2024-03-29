6797d11bf08d8832d52f12192bd2ca92e125aa5f
Sandro <31536101+sandromund@users.noreply.github.com>
2024-03-29 21:26:22 +0100

    Custom Loss function

--- src/const.py
+++ src/const.py
@@ -1,4 +1,4 @@
-LATENT_SPACE_SAMPLE = 100
+LATENT_SPACE_SAMPLE = 64
 ONE_HOT_ENCODING = True
 if ONE_HOT_ENCODING:
     DATA_DIMENSION = 36 * 36 * 4
@@ -6,4 +6,7 @@ else:
     DATA_DIMENSION = 36 * 36
 BETAS = (0.5, 0.999)  # decay of first and second  order momentum of gradient
 SEED = 42
-DISCRIMINATOR_DELAY = 5
+DISCRIMINATOR_DELAY = 0
+WEIGHT_BCE_LOSS = 0.2  # Weight for the BCELoss
+WEIGHT_CUSTOM_LOSS = 1 - WEIGHT_BCE_LOSS  # Weight for the custom loss function
+DISCRIMINATOR_ROUND_OUTPUT = False
--- src/train.py
+++ src/train.py
@@ -4,7 +4,7 @@ from torch import nn
 from torchmetrics.classification import BinaryF1Score, BinaryPrecision, BinaryRecall
 from tqdm import tqdm
 
-from const import LATENT_SPACE_SAMPLE, ONE_HOT_ENCODING, BETAS, SEED, DISCRIMINATOR_DELAY
+import const as c
 from data import get_data_loader
 from model import Generator, Discriminator
 
@@ -15,14 +15,19 @@ def train_models(data_path, batch_size, lr, num_epochs):
 
     with mlflow.start_run() as run:
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
         mlflow.set_tag("device", device)
+
         mlflow.log_param('batch_size', batch_size)
         mlflow.log_param('learning_rate', lr)
         mlflow.log_param('num_epochs', num_epochs)
-        mlflow.log_param("LATENT_SPACE_SAMPLE".lower(), LATENT_SPACE_SAMPLE)
-        mlflow.log_param("ONE_HOT_ENCODING".lower(), ONE_HOT_ENCODING)
-        mlflow.log_param("BETAS".lower(), BETAS)
-        mlflow.log_param("SEED".lower(), SEED)
+        mlflow.log_param("LATENT_SPACE_SAMPLE".lower(), c.LATENT_SPACE_SAMPLE)
+        mlflow.log_param("ONE_HOT_ENCODING".lower(), c.ONE_HOT_ENCODING)
+        mlflow.log_param("BETAS".lower(), c.BETAS)
+        mlflow.log_param("SEED".lower(), c.SEED)
+        mlflow.log_param("WEIGHT_BCE_LOSS".lower(), c.WEIGHT_BCE_LOSS)
+        mlflow.log_param("WEIGHT_CUSTOM_LOSS".lower(), c.WEIGHT_CUSTOM_LOSS)
+        mlflow.log_param("DISCRIMINATOR_ROUND_OUTPUT".lower(), c.DISCRIMINATOR_ROUND_OUTPUT)
 
         discriminator = Discriminator().to(device)
         generator = Generator().to(device)
@@ -32,19 +37,20 @@ def train_models(data_path, batch_size, lr, num_epochs):
         recall = BinaryRecall().to(device)
         precision = BinaryPrecision().to(device)
 
-        optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=BETAS)
-        optimizer_generator = torch.optim.Adam(generator.parameters(), lr=lr, betas=BETAS)
+        optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=c.BETAS)
+        optimizer_generator = torch.optim.Adam(generator.parameters(), lr=lr, betas=c.BETAS)
 
         delay_counter = 0
         for epoch in tqdm(range(num_epochs), "train_models"):
             for n, (real_samples, _) in enumerate(train_loader):
+                real_samples = real_samples.to(device=device)
                 delay_counter += 1
                 # Data for training the discriminator
                 real_samples_labels = torch.ones((batch_size, 1)).to(device=device)
-                latent_space_samples = torch.randn((batch_size, LATENT_SPACE_SAMPLE)).to(device=device)
+                latent_space_samples = torch.randn((batch_size, c.LATENT_SPACE_SAMPLE)).to(device=device)
                 generated_samples = generator(latent_space_samples).to(device=device)
                 generated_samples_labels = torch.zeros((batch_size, 1)).to(device=device)
-                all_samples = torch.cat((real_samples.to(device=device), generated_samples)).to(device=device)
+                all_samples = torch.cat((real_samples, generated_samples)).to(device=device)
                 all_samples_labels = torch.cat((real_samples_labels, generated_samples_labels)).to(device=device)
 
                 # Training the discriminator
@@ -52,19 +58,23 @@ def train_models(data_path, batch_size, lr, num_epochs):
                 output_discriminator = discriminator(all_samples).to(device=device)
 
                 loss_discriminator = loss_function(output_discriminator, all_samples_labels)
-                if delay_counter > DISCRIMINATOR_DELAY:
+                if delay_counter > c.DISCRIMINATOR_DELAY:
                     loss_discriminator.backward()
                     optimizer_discriminator.step()
                     delay_counter = 0
 
                 # Data for training the generator
-                latent_space_samples = torch.randn((batch_size, LATENT_SPACE_SAMPLE)).to(device=device)
+                latent_space_samples = torch.randn((batch_size, c.LATENT_SPACE_SAMPLE)).to(device=device)
 
                 # Training the generator
                 generator.zero_grad()
                 generated_samples = generator(latent_space_samples).to(device=device)
                 output_discriminator_generated = discriminator(generated_samples).to(device=device)
-                loss_generator = loss_function(output_discriminator_generated, real_samples_labels)
+
+                bce_loss_loss_generator = loss_function(output_discriminator_generated, real_samples_labels)
+                custom_loss_generator = torch.mean(torch.abs(output_discriminator_generated -
+                                                             torch.zeros_like(output_discriminator_generated)))
+                loss_generator = c.WEIGHT_BCE_LOSS * bce_loss_loss_generator + c.WEIGHT_CUSTOM_LOSS * custom_loss_generator
                 loss_generator.backward()
                 optimizer_generator.step()
 
@@ -89,6 +99,8 @@ def train_models(data_path, batch_size, lr, num_epochs):
                               recall(output_discriminator_generated, real_samples_labels), step=epoch)
             mlflow.log_metric("loss_discriminator", loss_discriminator, step=epoch)
             mlflow.log_metric("loss_generator", loss_generator, step=epoch)
+            mlflow.log_metric("loss_bce_generator", bce_loss_loss_generator, step=epoch)
+            mlflow.log_metric("loss_custom_generator", custom_loss_generator, step=epoch)
             mlflow.log_metric("sum_generated_samples", generated_samples.sum(), step=epoch)
             mlflow.log_metric("sum_real_samples", real_samples.sum(), step=epoch)
 
